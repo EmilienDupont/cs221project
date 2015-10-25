47a8399ff6937a738257620215181edbbb8df2a1
Dash <dashbodington@gmail.com>
2015-10-24 19:34:45 -0700

    Added mode calculation #ALaMode

--- training.py
+++ training.py
@@ -1,5 +1,6 @@
 import json
 import os.path
+import collections
 
 class Training:
     """
@@ -31,6 +32,18 @@ class Training:
         """
         return sum( float(review['stars']) for review in self.data )/self.numLines
 
+    def modeRating(self):
+        """
+        Calculates most common rating of reviews
+        """
+        C = collections.Counter()
+        for review in self.data:
+            s = review['stars']
+            C.update({s:1})
+        return C.most_common(1)[0][0]
+
 train = Training('../../../../../yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json',10000)
 #train.letMeSeeThatData()
 print 'Average rating of %s reviews is %s' % (train.numLines, train.averageRating())
+
+print 'Mode rating of %s reviews is %s' % (train.numLines, train.modeRating())
