bd7c371a2c9db3be6d896b3920ce028dfc7df466
William Zhang <wzhang4@stanford.edu>
2015-10-25 14:36:34 -0700

    added linear predictor function

--- training.py
+++ training.py
@@ -1,7 +1,8 @@
+import collections
 import json
 import os.path
-import collections
 import sys
+from utility import *
 
 # Analysing command line arguments
 if len(sys.argv) < 2:
@@ -11,18 +12,29 @@ if len(sys.argv) < 2:
 
 inputFile = sys.argv[1]
 
+def extractWordFeatures(x):
+    words, features = x.split(), {}
+    for word in words:
+        if (word in features):
+            features[word] += 1
+        else:
+            features[word] = 1
+    return features
+
 class Training:
     """
     Class to load and train data.
     """
-    def __init__(self, filename, numLines=100):
+    def __init__(self, filename, numLines=100, featureExtractor = extractWordFeatures):
         """
         Loads in json data from file at filename.
         """
         if not os.path.isfile(filename):
             raise RuntimeError, "The file '%s' does not exist" % filename
-
+        
+        self.filename = filename
         self.numLines = numLines
+        self.featureExtractor = extractWordFeatures
         self.data = []
 
         lineNum = 0
@@ -50,10 +62,41 @@ class Training:
             s = review['stars']
             C.update({s:1})
         return C.most_common(1)[0][0]
+    
+    def learnPredictor(self):
+        """
+        Learns a linear predictor based on the featureExtractor
+        """
+        self.weights = {}
+        
+        numIters, eta = 20, 0.025
+        for t in range(numIters):
+            for review in self.data:
+                star = review['stars']
+                text = review['text']
+                phi = self.featureExtractor(text)
+                
+                coeff = dotProduct(self.weights, phi)
+                increment(self.weights, float(-eta), phi)
+    
+    def predictRating(self, review):
+        """
+        Predicts a star rating from 1 to 5 given the |review| text
+        """
+        phi = self.featureExtractor(review['text'])
+        prediction = dotProduct(phi, self.weights)
+        if prediction <= 1:
+            return 1
+        elif prediction >= 5:
+            return 5
+        else:
+            return round(prediction)
 
 #train = Training('../../../../../yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json',10000)
-train = Training(inputFile)
+train = Training(inputFile, 1000)
 #train.letMeSeeThatData()
 print 'Average rating of %s reviews is %s' % (train.numLines, train.averageRating())
 
 print 'Mode rating of %s reviews is %s' % (train.numLines, train.modeRating())
+
+train.learnPredictor()
