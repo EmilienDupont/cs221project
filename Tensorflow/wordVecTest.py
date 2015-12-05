ec2b09341c45f11cd10d385823e138dabbe84ad9
William Zhang <wzhang4@stanford.edu>
2015-12-05 15:44:08 -0800

    trying embedding with negation taken into account

--- Tensorflow/wordVecTest.py
+++ Tensorflow/wordVecTest.py
@@ -1,13 +1,14 @@
-
 #from word2vec import *
 from encodeWords import *
 import tensorflow.python.platform
 import collections
 import numpy as np
 import tensorflow as tf
+import re
 import sys
 sys.path.insert(0, '../')
 from data import *
+from features import tbl, negation_patterns, neg_match, neg_match, punct_patterns, removePunctuation, punct_mark
 
 # Analyzing command line arguments
 if len(sys.argv) < 2:
@@ -32,3 +33,35 @@ final_embeddings, reverse_dictionary = encodeWords(vocab, 1000, 128, 1)
 
 print final_embeddings
 print reverse_dictionary
+
+def wordsWithNegation(leafWords):
+  def stem(word):
+    for leaf in leafWords:
+      if word[-len(leaf):] == leaf:
+	return word[:-len(leaf)]
+      else:
+	return word
+      
+  def extractor(text):
+    processedWords = []
+    prevNeg = False
+    for word in text.split():
+      wordStripped = stem(removePunctuation(word))
+      if not prevNeg:
+	prevNeg = (neg_match(word) != None)
+	processedWords.append(wordStripped)
+      else:
+	negWord = wordStripped + "_NEG"
+	processedWords.append(negWord)
+      if punct_mark(word):
+	prevNeg = False
+    return processedWords
+  
+  return extractor
+
+# dummy test
+test = unicode("I was not pleased with the service, but i was pleased with the food.")
+yolo = wordsWithNegation(['s','es','ed','er','ly','ing'])
+swag = yolo(test)
+
+print swag
