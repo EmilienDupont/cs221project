09b40bf76596ad99b580c4eec016347788511805
Dashiell Bodington <Dash@DN51slcd.SUNet>
2015-12-09 14:19:44 -0800

    Changed some things...

--- Tensorflow/word2vec.py
+++ Tensorflow/word2vec.py
@@ -25,28 +25,6 @@ from six.moves import urllib
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 import zipfile
-# Step 1: Download the data.
-
-def maybe_download(filename, expected_bytes):
-  """Download a file if not present, and make sure it's the right size."""
-  if not os.path.exists(filename):
-    filename, _ = urllib.request.urlretrieve(url + filename, filename)
-  statinfo = os.stat(filename)
-  if statinfo.st_size == expected_bytes:
-    print('Found and verified', filename)
-  else:
-    print(statinfo.st_size)
-    raise Exception(
-        'Failed to verify ' + filename + '. Can you get to it with a browser?')
-  return filename
-
-# Read the data into a string.
-def read_data(filename):
-  f = zipfile.ZipFile(filename)
-  for name in f.namelist():
-    return f.read(name).split()
-  f.close()
-#Read in our own words here
 
 # Step 2: Build the dictionary and replace rare words with UNK token.
 
