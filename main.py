abd6de091c3c2967f425dd682ed021cc842d600f
EmilienDupont <emilien.dupont@ymail.com>
2015-12-11 17:28:51 -0800

    main that runs all models

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import features
 
 from data import *
 from linearRegression import *
+from multiClassSVM import *
 from naiveBayes import *
 
 # Analyzing command line arguments
@@ -13,87 +14,32 @@ if len(sys.argv) < 2:
 inputFile = sys.argv[1]
 
 leafWords = ['s','es','ed','er','ly','ing']
-cw = features.readCommonWords('../data/common_words.txt')
+cw = features.readCommonWords('common_words.txt')
 
 # Import Data
-reviews = Data(inputFile, numLines = 1000, testLines = 100)
-
-# Without shuffling
-"""
-print "No shuffling"
-
-# Set up Naive Bayes model
-print "Naive Bayes"
-print "Word features:"
-naiveBayesModel = NaiveBayes(reviews, features.wordFeatures)
-naiveBayesModel.getInfo()
-
-print "With stemmed word features"
-naiveBayesModel.setNewFeatureExtractor(features.stemmedWordFeatures(leafWords))
-naiveBayesModel.getInfo()
-
-print "Without most common words"
-noCommonWords = features.wordFeaturesNoCommonWords(cw)
-naiveBayesModel.setNewFeatureExtractor(noCommonWords)
-naiveBayesModel.getInfo()
-
-# Set up linear model
-print "Linear model"
-linearModel = LinearRegression(reviews, features.wordFeatures)
-print '\n'
-print "Using word features:"
-linearModel.getInfo()
-
-# Change featureExtractor
-linearModel.setNewFeatureExtractor(features.stemmedWordFeatures(leafWords))
-print "Using stemmed word features:"
-linearModel.getInfo()
-
-# Without common words
-#print 'Please enter path to file with most common words: '
-#cwFile = input()
-#cw = features.readCommonWords(cwFile)
-noCommonWords = features.wordFeaturesNoCommonWords(cw)
-linearModel.setNewFeatureExtractor(noCommonWords)
-linearModel.getInfo()
-"""
-
-# same things but with shuffling enabled
-print "With Shuffling:"
+reviews = Data(inputFile, numLines = 50000, testLines = 5000)
+reviews.getInfo()
 reviews.shuffle()
 
-# Set up Naive Bayes model
-print "Naive Bayes"
-print "Word features:"
-naiveBayesModel = NaiveBayes(reviews, features.wordFeatures)
+
+# Different feature extractors
+f1 = features.posNegClusterFeatures("embeddings.p", "dictionary.p", 'NRC-Emotion-Lexicon-v0.92/NRC-emotion-lexicon-wordlevel-alphabetized-v0.92.txt', 200)
+f2 = features.wordFeatures
+f3 = features.positiveNegativeCountsWithClause('NRC-Emotion-Lexicon-v0.92/NRC-emotion-lexicon-wordlevel-alphabetized-v0.92.txt')
+f4 = features.emotionCounts('NRC-Emotion-Lexicon-v0.92/NRC-emotion-lexicon-wordlevel-alphabetized-v0.92.txt')
+f5 = features.stemmedWordFeatures(leafWords)
+f6 = features.clusterFeatures("embeddings.p", "dictionary.p", 200)
+f7 = features.wordFeaturesWithNegation(cw, leafWords)
+
+# SVM
+SVMModel = SVM(reviews, [f1, f5])
+SVMModel.getInfo()
+
+# Naive Bayes
+naiveBayesModel = NaiveBayes(reviews, [f3, f4, f5])
 naiveBayesModel.getInfo()
 
-print "With stemmed word features"
-naiveBayesModel.setNewFeatureExtractor(features.stemmedWordFeatures(leafWords))
-naiveBayesModel.getInfo()
-
-print "Without most common words"
-noCommonWords = features.wordFeaturesNoCommonWords(cw)
-naiveBayesModel.setNewFeatureExtractor(noCommonWords)
-naiveBayesModel.getInfo()
-
-# Set up linear model
-print "Linear model"
-linearModel = LinearRegression(reviews, features.wordFeatures)
-print '\n'
-print "Using word features:"
+# Linear Regression
+linearModel = LinearRegression(reviews, [f7, f3, f1])
 linearModel.getInfo()
 
-# Change featureExtractor
-linearModel.setNewFeatureExtractor(features.stemmedWordFeatures(leafWords))
-print "Using stemmed word features:"
-linearModel.getInfo()
-
-# Without common words
-#print 'Please enter path to file with most common words: '
-#cwFile = input()
-#cw = features.readCommonWords(cwFile)
-print "Without common words"
-noCommonWords = features.wordFeaturesNoCommonWords(cw)
-linearModel.setNewFeatureExtractor(noCommonWords)
-linearModel.getInfo()
